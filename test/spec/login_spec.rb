2828f0cdcbd94c228224b81e6c72e2eeaf7ae285
jefferson <jefferson.mello8@hotmail.com>
2019-11-14 17:15:48 -0300

    Refatorei o projeto utilizando page objects

--- test/spec/login_spec.rb
+++ test/spec/login_spec.rb
@@ -1,21 +1,21 @@
 require 'selenium-webdriver'
+require_relative './pages/login'
+
 
 describe 'Login' do
-  
-  before :each do
-    @driver = Selenium::WebDriver.for :chrome
-  end
 
-  after :each do
+  before(:each) do
+    @driver = Selenium::WebDriver.for :chrome
+    @login = Login.new(@driver)
+  end
+  
+  after(:each) do
     @driver.quit
   end
-
-  it 'succeeded' do
-    @driver.get 'https://intense-peak-03612.herokuapp.com//'
-    @driver.find_element(id: 'username').send_keys 'username'
-    @driver.find_element(id: 'password').send_keys 'password'
-    @driver.find_element(id: 'login').submit
-    expect(@driver.find_element(css: '.success').displayed?).to be(true)
+ 
+  it 'Caso feliz' do
+    @login.with('Jefferson', '1@2b3c')
+    expect(@login.success_message_present?).to be(true)
   end
 
 end
